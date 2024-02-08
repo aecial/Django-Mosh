641f0286561888e1bcca3d587b03264d8d0deec7
Teddy Pascual <teddylayson2000@gmail.com>
2024-02-08 20:24:31 +0800

    reset mosh

--- playground/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-# Like Routes in Express
-# URLConf
-# always end with forward slash
-urlpatterns = [
-    path('hello/', views.say_hello)
-]
--- playground/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-# Views are like endpoint functions
-def say_hello(request):
-    # can return html element
-    
-    return HttpResponse('<h1>Hello World </h1>')
