2d751ae4f952cd3caa6092dd4322585b08406846
Teddy Pascual <teddylayson2000@gmail.com>
2024-02-11 20:42:29 +0800

    added models and dynamic route for room pages

--- base/views.py
+++ base/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .models import Room
 # Create your views here.
-rooms =[
-    {'id':1, 'name': 'Lets learn Python'},
-    {'id':2, 'name': 'Lets learn Java'},
-    {'id':3, 'name': 'Lets learn C#'},
-]
+# rooms =[
+#     {'id':1, 'name': 'Lets learn Python'},
+#     {'id':2, 'name': 'Lets learn Java'},
+#     {'id':3, 'name': 'Lets learn C#'},
+# ]
 def say_hello(request):
     return JsonResponse({'message': 'I am Ted'})
 def romr(request):
+    # ORM To get all
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 #pk for dynamic route
 def room(request, pk):
     # Check for the room number
-    for room in rooms:
-       if room['id'] == int(pk):
-           myRoom = room                
-    context = {'id' : myRoom}     
+    rooms = Room.objects.get(id=pk)
+    context = {'information': rooms}   
     return render(request, 'base/room.html', context)
