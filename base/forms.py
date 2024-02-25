73850bd1deab87dbea3d56a87b15c371689d1f12
Teddy Pascual <teddylayson2000@gmail.com>
2024-02-25 23:41:37 +0800

    edit style of automatic forms by using widgets and tweaking templates

--- base/forms.py
+++ base/forms.py
@@ -1,11 +1,15 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from .models import Room, Message
-
+from django import forms
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         # Create all fields from the form model => __all__
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'}),
+            'description': forms.Textarea(attrs={'class': 'block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'})
+        }
 
 class MessageForm(ModelForm):
     class Meta:
